7ea903ce6ee2f87ec11b4232e7cb94c9f71fcac4
zmjjkk98 <ysyt3199@gmail.com>
2023-10-26 17:28:19 +0900

    로컬스토리지 구현

--- src/detail.js
+++ src/detail.js
@@ -175,18 +175,18 @@ function minToHourMin(min) {
 }
 
 
+
+
 // 관람평
 
 
-//테스트용 버튼
-const btn = document.querySelector("#btn")
-
-//버튼 클릭시 생성될 영역
-const section = document.querySelector("section");
+// localstorage에 저장된 pwd담을 배열 =(key값)
+let userInfo = []
 
 
 //ul태그
-const reviewUl = document.getElementById("ul");
+const reviewUl = document.querySelector("#reviewList");
+
 
 
 //form 태그에 정보 담아서 localstorage에 저장
@@ -195,11 +195,12 @@ const reviewInput = document.querySelector(".reviewSubmit input");
 
 
 // loginReviewPoint도 만들어야됨
-
 const login = document.querySelector("#login");
 const loginId = document.querySelector("#login .loginId");
 const loginPwd = document.querySelector("#login .loginPwd");
 const loginReview = document.querySelector("#login .review");
+const loginReviewPoint = document.querySelector("#login .reviewPoint")
+
 
 //클래스에 들어갈 변수 모음
 let id = "";
@@ -207,25 +208,30 @@ let pwd = "";
 let reviewPoint = "";
 let review = "";
 
-let count = 0;
+
+//감상평 작성된것들 불러올 때 쓸 변수
+const userIdElement = document.getElementById("userId");
+const userPwdElement = document.getElementById("userPwd");
 
 
 function onLogin(event) {
-  //event.preventDefault();
-  
+  event.preventDefault();                         //새로고침 막기
   id = loginId.value;
   pwd = loginPwd.value;
-  review = loginReview.value;
+  review = loginReview.textContent;
+  reviewPoint = loginReviewPoint.value;
   //window.localStorage.setItem(id, pwd);  
+
   
 
   //버튼 누르면 count++ 
   // count 값을 기준으로 반복문 
   
-  let user1 = new Review(id, pwd, review);
+  let user = new Review(id, pwd, review, reviewPoint);
 
-  window.localStorage.setItem(pwd, user1);
-  console.log(user1);
+  // window.localStorage.setItem(pwd, user1); 객체 저장
+  window.localStorage.setItem(pwd, JSON.stringify(user));   
+
 }
 
 login.addEventListener("submit", onLogin);
@@ -242,27 +248,44 @@ class Review {
 
 //local Storage에 있는 데이터 불러서 li로 만들기
 //여기부터 만들면 됨
+// 객체배열을 가지고오기? 
+// 객체 배열을 그려주기  
+// 객체 배열을 가지고 온다음에  그려줘야 할듯
+
+
+//새로고침을 해주는게 있으면 좋지 않을까?
 function drawReview(){
-    let drawTemp ="";
     
+    let drawTemp ="";
+    reviewUl.innerHTML += drawTemp;
+
+
+    
+
     for(let i=0; i<window.localStorage.length; i++){
+        
+        let usr = JSON.parse(window.localStorage.getItem(window.localStorage.key(i)));
+       
         drawTemp = 
-        `<li>
-        <div>
-        <li>
+        
+        `<li><div id="userId">${usr.id}</div>
+           <div id="userPwd">${usr.pwd}</div>
+            <div id="userReviwPoint>${usr.reviewPoint}</div>
+           <div id="userReview">${usr.review}</div>
+
         `
         
-    }
+        reviewUl.innerHTML += drawTemp;
+    }   
 
+
+    // window.localStorage.getItem("");
     // localStorage.setItem(,JSON.stringify());
 
-    
 
 }
 
-
-
-
+drawReview();
 
 //localStorage를 사용하기 위해선 변수가 필요할 듯?
 // 아이디 비번 (key, value)로 저장
